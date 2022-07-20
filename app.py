e777755aac609b923595ba9580604bfa2a53dd87
Dan Poynor <danpoynor@gmail.com>
2022-07-20 11:57:24 -0500

    Added add_book()

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from calendar import c
 from turtle import title
 from models import (Base, session, Book, engine)
 import datetime
 import csv
+import time
 
 
 def menu():
@@ -29,17 +31,37 @@ def clean_date(date_str):
     months = ['January', 'February', 'March', 'April', 'May', 'June',
               'July', 'August', 'September', 'October', 'November', 'December']
     split_date = date_str.split(' ')
-    # print(split_date)
-    month = int(months.index(split_date[0]) + 1)
-    day = int(split_date[1].split(',')[0])
-    year = int(split_date[2])
-    return datetime.date(year, month, day)
+    try:
+        month = int(months.index(split_date[0]) + 1)
+        day = int(split_date[1].split(',')[0])
+        year = int(split_date[2])
+    except ValueError:
+        input('''
+              \n***** DATE ERROR *****
+              \rThe date format should include a valid month, day, and year from the past.
+              \rEx: January 13, 2003
+              \rPress enter to try again.
+              \r**********************
+              ''')
+        return  # Same as return None
+    else:
+        return datetime.date(year, month, day)
 
 
 def clean_price(price_str):
-    price_float = float(price_str)
-    # return '${:,.2f}'.format(price_float)
-    return int(price_float * 100)
+    try:
+        price_float = float(price_str)
+    except ValueError:
+        input('''
+              \n***** PRICE ERROR *****
+              \rThe price format should be a number without a currency symbol.
+              \rEx: 12.99
+              \rPress enter to try again.
+              \r***********************
+              ''')
+        return
+    else:
+        return int(price_float * 100)
 
 
 def add_csv():
@@ -57,6 +79,31 @@ def add_csv():
                 session.add(new_book)
         session.commit()
 
+
+def add_book():
+    title = input('Title: ')
+    author = input('Author: ')
+    date_error = True
+    while date_error:
+        date = input('Published (Ex: October 25, 2017): ')
+        date_cleaned = clean_date(date)
+        if type(date_cleaned) == datetime.date:
+            date_error = False
+    price_error = True
+    while price_error:
+        price = input('Price: (Ex: 12.99): ')
+        price_cleaned = clean_price(price)
+        if type(price_cleaned) == int:
+            price_error = False
+    new_book = Book(
+        title=title,
+        author=author,
+        publish_date=date_cleaned,
+        price=price_cleaned)
+    session.add(new_book)
+    session.commit()
+    print(f'\n{title} by {author} added to the database.')
+    time.sleep(1.5)
 
 def app():
     app_running = True
@@ -77,10 +124,8 @@ def app():
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    # app()
     add_csv()
-    # print(clean_date('January 1, 2019'))
-    # print(clean_price('28.84'))
+    app()
 
     for book in session.query(Book).all():
         print(book)
